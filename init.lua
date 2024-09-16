c163d984e68d9389b6052c353d3f5a41d8d6c335
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2024-09-16 18:58:53 +0300

    lua ls

--- init.lua
+++ init.lua
@@ -615,7 +615,13 @@ require('lazy').setup({
                 callSnippet = 'Replace',
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                globals = {
+                  'vim',
+                  'require',
+                },
+                disable = { 'missing-fields' },
+              },
             },
           },
         },

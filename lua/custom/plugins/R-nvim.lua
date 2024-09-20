2d5ef5355fe39063c15036f048a0939b5c17da04
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2024-09-20 21:44:10 +0300

    R.nvim

--- lua/custom/plugins/R-nvim.lua
+++ lua/custom/plugins/R-nvim.lua
@@ -1,6 +1,10 @@
 -- R Support
 return {
-  "R-nvim/R.nvim",
-  lazy = false
+  'R-nvim/R.nvim',
+  lazy = false,
+  config = function()
+    local options = {}
+    require('r').setup(options)
+  end,
 }
 -- vim: ts=2 sts=2 sw=2 et

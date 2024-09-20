97630b419dc7b7075be4f4eed3c26afb9fff1698
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2024-09-20 21:44:44 +0300

    tinymist

--- init.lua
+++ init.lua
@@ -610,6 +610,19 @@ require('lazy').setup({
         --
         tailwindcss = {},
         zls = {},
+        tinymist = {
+          single_file_support = true,
+          root_dir = function()
+            return vim.fn.getcwd()
+          end,
+          settings = {
+            tinymist = {
+              -- rootPath = '-',
+              exportPdf = 'onType',
+              outputPath = '$root/target/$name',
+            },
+          },
+        },
 
         lua_ls = {
           -- cmd = {...},

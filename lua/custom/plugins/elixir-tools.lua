2dbd8021a87a02c3b00b204cf90be94a2c3c74ea
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2024-09-21 19:54:05 +0300

    Elixir fiddling

--- lua/custom/plugins/elixir-tools.lua
+++ lua/custom/plugins/elixir-tools.lua
@@ -8,18 +8,22 @@ return {
 
     elixir.setup {
       nextls = { enable = true },
-      credo = {},
+      credo = { enable = true },
       elixirls = {
         enable = true,
         settings = elixirls.settings {
           dialyzerEnabled = false,
           enableTestLenses = false,
+          suggestSpecs = false,
         },
         on_attach = function(client, bufnr)
           vim.keymap.set('n', '<space>fp', ':ElixirFromPipe<cr>', { buffer = true, noremap = true })
           vim.keymap.set('n', '<space>tp', ':ElixirToPipe<cr>', { buffer = true, noremap = true })
           vim.keymap.set('v', '<space>em', ':ElixirExpandMacro<cr>', { buffer = true, noremap = true })
         end,
+      },
+      projectionist = {
+        enable = true,
       },
     }
   end,

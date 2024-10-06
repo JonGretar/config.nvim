16801bc4a465770ce6e0ce325faf3267d79334e4
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2024-10-06 14:17:48 +0300

    R-nvim changes

--- lua/custom/plugins/R-nvim.lua
+++ lua/custom/plugins/R-nvim.lua
@@ -3,7 +3,16 @@ return {
   'R-nvim/R.nvim',
   lazy = false,
   config = function()
-    local options = {}
+    local options = {
+      is_darwin = false,
+      external_term = 'wezterm cli split-pane --bottom --percent 30  -- ',
+      auto_quit = true,
+      R_app = 'radian',
+      bracketed_paste = true,
+
+      pipe_keymap = '',
+      assignment_keymap = '',
+    }
     require('r').setup(options)
   end,
 }

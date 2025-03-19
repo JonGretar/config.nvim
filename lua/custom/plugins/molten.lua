f707598486b305aadd953c793795122384a70f82
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2025-03-19 14:14:50 +0200

    Molten config for wezterm

--- lua/custom/plugins/molten.lua
+++ lua/custom/plugins/molten.lua
@@ -5,5 +5,8 @@ return {
   init = function()
     -- this is an example, not a default. Please see the readme for more configuration options
     vim.g.molten_output_win_max_height = 12
+    vim.g.molten_image_provider = 'wezterm'
+    vim.g.molten_auto_open_output = false
+    -- vim.keymap.set('n', '<localleader>e', ':MoltenEvaluateOperator<CR>', { silent = true, desc = 'run operator selection' })
   end,
 }
